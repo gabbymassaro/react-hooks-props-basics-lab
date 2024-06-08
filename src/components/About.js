9469537a68df49b351c08afc028b5a32458f15cd
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-08 17:35:06 -0400

    Finish the coniditional rendering of <p> tag

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,15 @@ import React from "react";
 import Links from "./Links"
 
 function About(props) {
+  let bioTag = undefined
+  if (props.bio !== undefined && props.bio !== "") {
+    bioTag = <p>{props.bio}</p>
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {bioTag}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github = {props.links.github} linkedin = {props.links.linkedin}/>
     </div>
